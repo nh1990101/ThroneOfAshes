4a6e248dbc1903573361708c0b12e2654316a9e7
ztl <837888738@qq.com>
2026-09-23 15:19:46 +0800

    配置示例

--- assets/script/Config/ConfigMgr.ts
+++ assets/script/Config/ConfigMgr.ts
@@ -1,6 +1,8 @@
 import { _decorator, Asset, Component, JsonAsset, loader, Node, resources } from 'cc';
 import { DEBUG } from 'cc/env';
 import { AssetMgr } from '../Common/AssetMgr';
+import { IStringChTbl } from './Typings/string_ch_tbl';
+import { IFunctionPropTbl } from './Typings/function_prop_tbl';
 const { ccclass, property } = _decorator;
 
 
@@ -8,6 +10,9 @@ const { ccclass, property } = _decorator;
 export class ConfigMgr {
     private zip: JSZip = null;
     private static _instance: ConfigMgr;
+
+    public string_ch_tbl = new Map<number, IStringChTbl>();
+    public function_prop_tbl = new Map<number, IFunctionPropTbl>();
     /**主键调整表 
     * 表名：主键字符串
    */
@@ -22,6 +27,7 @@ export class ConfigMgr {
         }
         return this._instance;
     }
+
     async loadConfigZip() {
         return new Promise<void>(async (resolve, reject) => {
             try {
